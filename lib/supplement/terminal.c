0b565ad104da4bc16411786d1160a4a3021bc6ca
Bertram Scharpf <software@bertram-scharpf.de>
2025-10-23 10:16:15 +0200

    Remove Ruby 3.2 IO functions

--- lib/supplement/terminal.c
+++ lib/supplement/terminal.c
@@ -13,14 +13,6 @@ static VALUE io_unget( VALUE);
 static VALUE io_reset( VALUE);
 
 
-#ifdef FEATURE_OLD_IO_FUNCTIONS
-  #define RB_FAIL_PATH( io, fptr)   ((fptr)->pathv)
-  #define RB_IO_VAR(    io, fptr)   fptr
-#else
-  #define RB_FAIL_PATH( io, fptr)   rb_io_path( io)
-  #define RB_IO_VAR(    io, fptr)   io
-#endif
-
 /*
  *  call-seq:
  *     io.unget(str, ...)   -> nil
@@ -33,28 +25,20 @@ static VALUE io_reset( VALUE);
 VALUE
 rb_io_unget( int argc, VALUE *argv, VALUE io)
 {
-#ifdef FEATURE_OLD_IO_FUNCTIONS
-    rb_io_t *fptr;
-#endif
     int fd;
     struct termios oldtio, newtio;
     void *v[5];
 
-#ifdef FEATURE_OLD_IO_FUNCTIONS
-    GetOpenFile( io, fptr);
-    fd = fptr->fd;
-#else
     fd = rb_io_descriptor( io);
-#endif
 
     if (tcgetattr( fd, &oldtio) < 0)
-        rb_sys_fail_str( RB_FAIL_PATH( io, fptr));
+        rb_sys_fail_str( rb_io_path( io));
     newtio = oldtio;
     newtio.c_iflag &= ~ICRNL;
     if (tcsetattr( fd, TCSANOW, &newtio) < 0)
-        rb_sys_fail_str( RB_FAIL_PATH( io, fptr));
+        rb_sys_fail_str( rb_io_path( io));
 
-    v[0] = &fd, v[1] = (void *) RB_IO_VAR( io, fptr), v[2] = &oldtio,
+    v[0] = &fd, v[1] = (void *) io, v[2] = &oldtio,
         v[3] = &argc, v[4] = (void *) argv;
     return rb_ensure( io_unget, (VALUE) v, io_reset, (VALUE) v);
 }
@@ -76,7 +60,7 @@ io_unget( VALUE v)
         p = RSTRING_PTR(str);
         for (i = RSTRING_LEN(str); i; --i, ++p)
             if (ioctl( *(int *) vp[0], TIOCSTI, p) < 0)
-                rb_sys_fail_str( RB_FAIL_PATH( (VALUE) vp[1], (rb_io_t *) vp[1]));
+                rb_sys_fail_str( rb_io_path( (VALUE) vp[1]));
     }
     return Qnil;
 }
@@ -87,7 +71,7 @@ io_reset( VALUE v)
     void **vp = (void **) v;
 
     if (tcsetattr( *(int *) vp[0], TCSANOW, (struct termios *) vp[2]) < 0)
-        rb_sys_fail_str( RB_FAIL_PATH( (VALUE) vp[1], (rb_io_t *) vp[1]));
+        rb_sys_fail_str( rb_io_path( (VALUE) vp[1]));
 
     return Qnil;
 }
@@ -105,22 +89,14 @@ io_reset( VALUE v)
 VALUE
 rb_io_wingeom( VALUE self)
 {
-#ifdef FEATURE_OLD_IO_FUNCTIONS
-    rb_io_t *fptr;
-#endif
     int fd;
     struct winsize w;
     VALUE r;
 
-#ifdef FEATURE_OLD_IO_FUNCTIONS
-    GetOpenFile( self, fptr);
-    fd = fptr->fd;
-#else
     fd = rb_io_descriptor( self);
-#endif
 
     if (ioctl( fd, TIOCGWINSZ, &w) < 0)
-        rb_sys_fail_str( RB_FAIL_PATH( self, fptr));
+        rb_sys_fail_str( rb_io_path( self));
     r = rb_ary_new2( 4);
     rb_ary_store( r, 0, INT2NUM( w.ws_col));
     rb_ary_store( r, 1, INT2NUM( w.ws_row));
